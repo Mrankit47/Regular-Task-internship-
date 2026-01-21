3e2d941b6d8a45126255cb05e681be6df0977679
Ankit Kushwah <ankitkushwah0210k@gmail.com>
2026-01-21 17:19:12 +0530

    Add files via upload

---
+++ Combinig.py
@@ -0,0 +1,5 @@
+def All_argument(Id,*data,**detail):
+    print("Id : ",Id)
+    print("Name : ",data)
+    print("all Detail ",detail)
+All_argument(420,"Ankit Kushwah",24, name="ankit", age= 24, com="EIC")
