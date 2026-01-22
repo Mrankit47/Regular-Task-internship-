00c91e44d7a569d70e7286fe965c28777fb8a5be
Ankit Kushwah <ankitkushwah0210k@gmail.com>
2026-01-22 18:23:39 +0530

    Add files via upload

---
+++ Q2List.py
@@ -0,0 +1,6 @@
+#last 3 element of the list
+
+num = [1,3,5,6,8,6,77,55,43,32]
+
+L =[x for x in num[-1:-4:-1]]
+print(L)
---
+++ QList1.py
@@ -0,0 +1,5 @@
+# creat a list of number from 1 to 10 and print the first 5 elements
+num = [1,3,5,6,8,6,77,55,43,32]
+
+p = [x for x in num[0:5]]
+print(p)
